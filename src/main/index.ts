f58ade070c24b71a4eb0f921d0098162644818f6
Jayden <2077780946@qq.com>
2025-05-09 18:33:30 +0800

    移除 electron-router-dom 相关代码，更新主进程和渲染进程的路由实现，使用 react-router-dom 的 BrowserRouter 进行路由管理。

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,7 @@
-import { app, shell, BrowserWindow, ipcMain } from "electron"
-import path, { join } from "path"
-import { electronApp, optimizer } from "@electron-toolkit/utils"
+import { electronApp, is, optimizer } from "@electron-toolkit/utils"
+import { app, BrowserWindow, ipcMain, shell } from "electron"
+import { join } from "path"
 import icon from "../../resources/icon.png?asset"
-import { registerRoute } from "../lib/electron-router-dom"
 
 function createWindow(): void {
   // Create the browser window.
@@ -14,18 +13,12 @@ function createWindow(): void {
     ...(process.platform === "linux" ? { icon } : {}),
     webPreferences: {
       preload: join(__dirname, "../preload/index.js"),
-      sandbox: false,
-      webSecurity: false
+      sandbox: false
     }
   })
 
   mainWindow.on("ready-to-show", () => {
     mainWindow.show()
-  })
-  registerRoute({
-    id: "main",
-    browserWindow: mainWindow,
-    htmlFile: path.join(__dirname, "../renderer/index.html")
   })
 
   mainWindow.webContents.openDevTools()
@@ -36,6 +29,11 @@ function createWindow(): void {
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
+  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"])
+  } else {
+    mainWindow.loadFile(join(__dirname, "../renderer/index.html"))
+  }
 }
 
 // This method will be called when Electron has finished
--- src/renderer/src/route.tsx
+++ src/renderer/src/route.tsx
@@ -1,19 +1,14 @@
-import { Router } from "../../lib/electron-router-dom"
-
-import { Route } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import MainScreen from "./MainScreen"
 import { Layout } from "./layout"
-
 export function AppRoutes() {
   return (
-    <Router
-      main={
-        <>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<MainScreen />} />
-          </Route>
-        </>
-      }
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<MainScreen />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
